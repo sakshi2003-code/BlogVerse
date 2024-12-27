92b5f6f0371e208906d03d59cf399c05f5312125
_sakshi <sakshibijalwan777@gmail.com>
2024-12-27 19:20:40 +0530

    conf folder setting

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,5 +1,4 @@
-// env variable sara string m hona chaiye 
-// export env variables efficiently
+
 const conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
@@ -7,6 +6,6 @@ const conf = {
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
-// there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
+
 
 export default conf
